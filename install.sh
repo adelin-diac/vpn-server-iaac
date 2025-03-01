d93113f3265bf90de5fbac0de1022260a8c3724f
adelin-diac <adelin334@gmail.com>
2025-03-01 08:19:05 +0000

    install script initialise

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "$1"
+
+# to get set up, people can do: curl <github-repo>/blob/main/install.sh | sh
+
+# TODO: run `sudo setup-docker.sh --add-user "$SERVER_USER"`
+# allow user to select if they want the script to attempt to automatically open a port on your router
