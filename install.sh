e335c99ad9ccdedc0d1e03f7ce9f7f59c99735f0
adelin-diac <adelin334@gmail.com>
2025-03-01 08:49:20 +0000

    clone repo in setup script

--- install.sh
+++ install.sh
@@ -3,9 +3,14 @@
 echo "Running setup script..."
 echo ""
 
+echo "Cloning repo..."
+git clone https://github.com/adelin-diac/vpn-server-iaac.git
+cd vpn-server-iaac
+echo ""
+
 echo "Installing docker..."
 sudo ./scripts/setup-docker.sh --add-user "$(whoami)"
-
+echo ""
 
 # Things that can be automatic:
 # - Port Forwarding (using upnp)
